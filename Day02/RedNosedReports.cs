8e1047efa37b03d3cc35441bfd1d1a256a6adb95
andr-be <andr-be@github.com>
2024-12-02 23:27:29 +0000

    completely refactored P1; finished P2

--- Day02/RedNosedReports.cs
+++ Day02/RedNosedReports.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2024.Day02
+﻿namespace AdventOfCode2024.Day02
 {
     internal class RedNosedReports : ISolution
     {
@@ -34,96 +28,51 @@ namespace AdventOfCode2024.Day02
             var safeCount = 0;
             foreach (var line in puzzleInput)
             {
-                var array = SplitIntoIntArray(line);
-                var safeGradient = ConsistentGradient(array, out var _);
-                var safeDistances = CheckSafeDistances(array);
-                if (safeGradient && safeDistances)
+                int[] array = SplitIntoIntArray(line);
+                int[] deltas = CalculateDeltas(array);
+                if (SafeSequence(deltas))
                 {
-                    Console.WriteLine($"{line} is SAFE");
+                    Console.WriteLine($"{line} is safe.");
                     safeCount++;
                 }
-                else
+                else 
                 {
-                    var reason = (safeGradient, safeDistances) switch
+                    var isUnsafe = true;
+                    for (int i = 0; i < array.Length && part is Part.Two; i++)
                     {
-                        (true, false) => "Consistent gradient; improper distances",
-                        (false, true) => "Inconsistent gradient, safe distances",
-                        (false, false) => "Both unsafe",
-                        _ => ""
-                    };
-                    Console.WriteLine($"{line} is UNSAFE: {reason}");
+                        var modifiedArray = array.Take(i).Concat(array.Skip(i + 1)).ToArray();
+                        if (SafeSequence(CalculateDeltas(modifiedArray)))
+                        {
+                            safeCount++;
+                            isUnsafe = false;
+                            break;
+                        }
+                    }
+                    Console.WriteLine(isUnsafe ? $"{line} is unsafe!" : $"{line} is safe with DAMPENER applied!");
                 }
-            }    
+            }
             return safeCount.ToString();
         }
 
-        private bool CheckSafeDistances(int[] array)
+        private static bool SafeSequence(int[] deltas)
         {
+            return deltas.All(d => d is <= -1 and >= -3)
+                || deltas.All(d => d is >= +1 and <= +3);
+        }
+
+        private int[] CalculateDeltas(int[] array)
+        {
+            List<int> deltas = [];
+            
             for (int i = 1; i < array.Length; i++)
-            {
-                var difference = Math.Abs(array[i] - array[i - 1]);
-                if (difference >= 1 && difference <= 3) continue;
-                else return false;
-            }
-            return true;
+                deltas.Add(array[i] - array[i - 1]);
+            
+            return [.. deltas];
         }
 
         private int[] SplitIntoIntArray(string line) => 
             line.Split(' ')
                 .Select(x => int.Parse(x.Trim()))
                 .ToArray();
-
-        private bool ConsistentGradient(int[] levels, out Direction direction)
-        {
-            direction = FindChangeDirection(levels);
-            if (direction == Direction.Flat)
-            {
-                return false;
-            }
-            if (direction == Direction.Downwards)
-            {
-                for (int i = 1; i < levels.Length; i++)
-                {
-                    if (levels[i] >= levels[i - 1]) 
-                        return false;
-                }
-            }
-            if (direction == Direction.Upwards)
-            {
-                for (int i = 1; i < levels.Length; i++)
-                {
-                    if (levels[i] <= levels[i - 1])
-                        return false;
-                }
-            }
-            return true;
-        }
-
-        private Direction FindChangeDirection(int[] levels)
-        {
-            Direction direction = Direction.Flat;
-            for (int i = 1; i < levels.Length; i++)
-            {
-                if (levels[i] < levels[i - 1])
-                {
-                    direction = Direction.Downwards;
-                    break;
-                }
-                else if (levels[i] > levels[i - 1])
-                {
-                    direction = Direction.Upwards;
-                    break;
-                }
-                else continue;
-            }
-            return direction;
-        }
-
-        private enum Direction
-        {
-            Upwards,
-            Downwards,
-            Flat
-        }
     }
 }
