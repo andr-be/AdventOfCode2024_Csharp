5d33132fbeb0f0af359171cb8d4a1b3ee05fc903
andr-be <andr-be@github.com>
2024-12-05 09:34:22 +0000

    Created Day 5: Print Queue

---
+++ Day05/PrintQueue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Day05
+{
+    internal class PrintQueue : SolutionBase
+    {
+        public override string Name => "Day 05: Print Queue";
+
+        public override int Day => 5;
+
+        public override string Solution(Part part, string input)
+        {
+            return "BLEGH";
+        }
+    }
+}
